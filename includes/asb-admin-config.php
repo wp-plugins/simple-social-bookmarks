29c993805c6afacdbae3d9d333d7f6a15edd8c5f
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-28 08:21:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-social-bookmarks/tags/3.2.1@524640 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/asb-admin-config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* Admin Menu Functions
+*
+* Various functions relating to the various administration screens
+*
+* @package	Artiss-Social-Bookmarks
+*/
+
+/**
+* Add meta to plugin details
+*
+* Add options to plugin meta line
+*
+* @since	3.2
+*
+* @param	string  $links	Current links
+* @param	string  $file	File in use
+* @return   string			Links, now with settings added
+*/
+
+function asb_set_plugin_meta( $links, $file ) {
+
+	if ( strpos( $file, 'simple-social-bookmarks.php' ) !== false ) {
+		$links = array_merge( $links, array( '<a href="http://www.artiss.co.uk/forum">' . __( 'Support' ) . '</a>' ) );
+		$links = array_merge( $links, array( '<a href="http://www.artiss.co.uk/donate">' . __( 'Donate' ) . '</a>' ) );
+	}
+
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'asb_set_plugin_meta', 10, 2 );
+?>
