15b1dc243816fa6aac6aa5a6ef034a296ac0d2e1
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-03 07:52:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-social-bookmarks/trunk@526616 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ simple-social-bookmarks.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Artiss Social Bookmarks
+Plugin URI: http://www.artiss.co.uk/social-bookmarks
+Description: Display links to social bookmark sites
+Version: 3.2.3
+Author: David Artiss
+Author URI: http://www.artiss.co.uk
+*/
+
+/**
+* Artiss Social Bookmarks
+*
+* Main code - include various functions
+*
+* @package	Artiss-Social-Bookmarks
+* @since	3.2
+*/
+
+define( 'artiss_social_bookmarks_version', '3.2.3' );
+
+$functions_dir = WP_PLUGIN_DIR . '/simple-social-bookmarks/includes/';
+
+include_once( $functions_dir . 'asb-shared-functions.php' );			        // Shared functions
+
+// Include all the various functions
+
+if ( is_admin() ) {
+
+    include_once( $functions_dir . 'asb-admin-config.php' );		            // Assorted admin configuration changes
+
+} else {
+
+    include_once( $functions_dir . 'asb-define-bookmarks.php' );			    // Set up the bookmark definitions
+
+	include_once( $functions_dir . 'asb-generate-bookmarks.php' );			    // Generate the bookmarks
+
+	include_once( $functions_dir . 'asb-shortcodes.php' );			            // Shortcode definitions
+
+	include_once( $functions_dir . 'asb-functions.php' );			            // Function calls
+
+	include_once( $functions_dir . 'asb-deprecated.php' );			            // Deprecated functions
+
+}
+?>
